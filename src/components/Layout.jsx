eb186c24bdd8328e4ce0ea46c49f42f6804e73b0
SimPresko <preslibankov@gmail.com>
2025-02-26 17:14:43 +0100

    - Add header & footer in layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,33 @@
 export default function Layout(props) {
   const { children } = props
 
-  const header = <header></header>
-  const footer = <footer></footer>
+  const header = (
+    <header>
+      <div>
+        <h1 className='text-gradient'>CAFFIEND</h1>
+        <p>For Coffee Insatiates</p>
+      </div>
+      <button>
+        <p>Sign up Free</p>
+        <i className='fa-solid fa-mug-hot'></i>
+      </button>
+    </header>
+  )
+  const footer = (
+    <footer>
+      <p>
+        <span className='text-gradient'>Caffiend</span> was made by{' '}
+        <a href='https://www.presiyan-bankov.com' target='_blank'>
+          PreZko
+        </a>{' '}
+        using the{' '}
+        <a href='https://www.fantacss.smoljames.com' target='_blank'>
+          FantaCSS{' '}
+        </a>
+        design library.
+      </p>
+    </footer>
+  )
   return (
     <>
       {header}
