8d0725cea33479ec76cc48b389256391ce8bbef3
SimPresko <preslibankov@gmail.com>
2025-02-27 19:23:12 +0100

    Version 3 - Add a coffee from for the client to fill out - Create history and stats components for authenticated users - Set up authentication conditioning - Create a modal that shows up and closes when you click outside

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,10 @@
+import { useState } from 'react'
+import Authentication from './Authentication'
+import Modal from './Modal'
+
 export default function Layout(props) {
   const { children } = props
+  const [showModal, setShowModal] = useState(false)
 
   const header = (
     <header>
@@ -7,7 +12,11 @@ export default function Layout(props) {
         <h1 className='text-gradient'>CAFFIEND</h1>
         <p>For Coffee Insatiates</p>
       </div>
-      <button>
+      <button
+        onClick={() => {
+          setShowModal(true)
+        }}
+      >
         <p>Sign up Free</p>
         <i className='fa-solid fa-mug-hot'></i>
       </button>
@@ -19,17 +28,35 @@ export default function Layout(props) {
         <span className='text-gradient'>Caffiend</span> was made by{' '}
         <a href='https://www.presiyan-bankov.com' target='_blank'>
           PreZko
-        </a>{' '}
-        using the{' '}
+        </a>
+        <br /> using the{' '}
         <a href='https://www.fantacss.smoljames.com' target='_blank'>
           FantaCSS{' '}
         </a>
         design library.
+        <br />
+        Check out the project on{' '}
+        <a
+          target='_blank'
+          href='https://github.com/PreZko/Coffee-Tracker-ReactJS'
+        >
+          GitHub
+        </a>
+        !
       </p>
     </footer>
   )
   return (
     <>
+      {showModal && (
+        <Modal
+          handleCloseModal={() => {
+            setShowModal(false)
+          }}
+        >
+          <Authentication />
+        </Modal>
+      )}
       {header}
       <main>{children}</main>
       {footer}
