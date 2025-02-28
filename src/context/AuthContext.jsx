fdad3f8e1b3479c5b1e2b9f1aff05e608315ef25
SimPresko <preslibankov@gmail.com>
2025-02-28 18:16:04 +0100

    Version 4 - Connect to Firebase - Make auth with Firebase - Make a DB with Firestore - Set up login/register routes

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,51 @@
+import {
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth'
+import { useState, useEffect, useContext, createContext } from 'react'
+import { auth } from '../../firebase'
+
+const AuthContext = createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+export function AuthProvider(props) {
+  const { children } = props
+  const [user, setUser] = useState(null)
+  const [globalData, setGlobalData] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  function resetPassword(email) {
+    return sendPasswordResetEmail(auth, email)
+  }
+
+  function logout() {
+    setUser(null)
+    setGlobalData(null)
+    return signOut(auth)
+  }
+
+  const value = {
+    user,
+    globalData,
+    setGlobalData,
+    isLoading,
+    signup,
+    login,
+    logout,
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
